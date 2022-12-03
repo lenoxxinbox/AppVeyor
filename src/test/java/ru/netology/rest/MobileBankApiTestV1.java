ae696bec9142f9a5226175fc43b6604272ff778d
Lenoxx in Box <lenoxxinbox@yandex.ru>
2022-12-03 14:11:49 +0300

    Add JSON schema

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -10,28 +10,15 @@ import static io.restassured.RestAssured.given;
 class MobileBankApiTestV1 {
     @Test
     void shouldReturnDemoAccounts() {
-        // Given - When - Then
-        // Предусловия
-//      given()
-//          .baseUri("http://localhost:9999/api/v1")
-//      // Выполняемые действия
-//      .when()
-//          .get("/demo/accounts")
-//      // Проверки
-//      .then()
-//          .statusCode(200);
-//    }
-        given()
-                .baseUri("http://localhost:9999/api/v1")
-                .when()
-                .get("/demo/accounts")
-                .then()
-                .statusCode(200)
-//                .header("Content-Type", "application/json; charset=UTF-8")
-                .contentType(ContentType.JSON)
-                .body("", hasSize(3))
-                .body("[1].balance", greaterThanOrEqualTo(0))
-                .body("every { it.balance >= 0 }", is(true))
-                .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
+
+      given()
+          .baseUri("http://localhost:9999/api/v1")
+
+      .when()
+          .get("/demo/accounts")
+
+      .then()
+          .statusCode(200);
     }
+
 }
